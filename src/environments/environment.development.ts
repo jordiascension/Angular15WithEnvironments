01f204f7d5faba7e27001bc3ce6e0908438be85c
jordiascension <jordiascension@enterpriseconsulting.es>
2023-03-31 13:38:52 +0200

    Added Example with environment variables

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    link:'http://development',
+    name:'development environment',
+    code: 1003
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    link:'',
+    name:'',
+    code:0
+};
